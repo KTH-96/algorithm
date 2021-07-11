c457614d1558ceadc49165401712452b19446e3c
KTH-96 <taehyun960@gmail.com>
2021-07-11 15:30:41 +0900

    commit test2

--- src/main/java/day0711/S1330.java
+++ src/main/java/day0711/S1330.java
@@ -2,6 +2,6 @@ package day0711;
 
 public class S1330 {
     public static void main(String[] args) {
-        System.out.println("if문 두수 비교하기");
+        System.out.println("if문 두수 비교하기!!");
     }
 }
