34408f2387d00b6f3c35d723b343cc42999032bc
KTH-96 <taehyun960@gmail.com>
2021-07-12 00:09:30 +0900

    삼항연산자 사용해보기

---
+++ src/main/java/day0711/S9498.java
@@ -0,0 +1,17 @@
+package day0711;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S9498 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int score = Integer.parseInt(br.readLine());
+
+        String result = (score >= 90) ? "A" : ((score >= 80) ? "B" : ( (score >= 70) ? "C" : (score >= 60) ? "D" : "F"));
+
+        System.out.println(result);
+    }
+}
