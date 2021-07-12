e56e96d9067da9a3f5f99f476af24fbc00858cc7
KTH-96 <taehyun960@gmail.com>
2021-07-12 22:36:48 +0900

    0712

---
+++ src/main/java/day0712/S2741.java
@@ -0,0 +1,20 @@
+package day0712;
+
+import java.io.*;
+
+public class S2741 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            bw.write(++c + "\n");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}
