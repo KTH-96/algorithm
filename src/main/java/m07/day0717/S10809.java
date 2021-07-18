5bf71432403705cda1c27f9fda8bceedf8fda8c9
KTH-96 <taehyun960@gmail.com>
2021-07-18 17:20:12 +0900

    ascii

---
+++ src/main/java/m07/day0717/S10809.java
@@ -0,0 +1,26 @@
+package m07.day0717;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S10809 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String S = br.readLine();
+        int[] arr =new int[26];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+        for (int i = 0; i < S.length(); i++) {
+            if (arr[S.charAt(i)-97] == -1) arr[S.charAt(i)-97] = i;
+        }
+        for (int i:arr) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+
+    }
+}
