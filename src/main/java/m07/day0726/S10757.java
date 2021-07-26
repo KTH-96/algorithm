1843163d62886d9052e49c049963aa773e08f17d
KTH-96 <taehyun960@gmail.com>
2021-07-26 11:34:03 +0900

    0726

---
+++ src/main/java/m07/day0726/S10757.java
@@ -0,0 +1,19 @@
+package m07.day0726;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class S10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BigInteger bi1 = new BigInteger(st.nextToken());
+        BigInteger bi2 = new BigInteger(st.nextToken());
+
+        System.out.println(bi1.add(bi2));
+
+    }
+}
