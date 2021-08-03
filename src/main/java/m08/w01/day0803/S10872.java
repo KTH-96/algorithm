84e7a5d44344871f8db4eb54729de32eabe6751a
KTH-96 <taehyun960@gmail.com>
2021-08-03 14:07:54 +0900

    0803

---
+++ src/main/java/m08/w01/day0803/S10872.java
@@ -0,0 +1,22 @@
+package m08.w01.day0803;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S10872 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        if (n == 0) {
+            n = 1;
+        }
+
+        int factory = 1;
+        for (int i = 1; i <= n; i++) {
+            factory *= i;
+        }
+        System.out.println(factory);
+    }
+}
