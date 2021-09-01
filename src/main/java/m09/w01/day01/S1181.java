3475fcebc85630fdeaa94be041308f3b61e931ec
KTH-96 <taehyun960@gmail.com>
2021-09-01 22:25:34 +0900

    이중 비교

---
+++ src/main/java/m09/w01/day01/S1181.java
@@ -0,0 +1,15 @@
+package m09.w01.day01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+
+
+    }
+}
