ae92b79fcde2bdb56c9036b05d3946f162517372
KTH-96 <taehyun960@gmail.com>
2021-10-03 12:28:08 +0900

    dfs

---
+++ src/main/java/m10/S15651.java
@@ -0,0 +1,37 @@
+package m10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S15651 {
+    static int[] arr;
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());//세로 갯수
+        M = Integer.parseInt(st.nextToken());//가로 갯수
+
+        arr = new int[M];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N ; i++) {
+            arr[depth] = i;
+            dfs(depth + 1);
+        }
+    }
+}
