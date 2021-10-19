0988c9ca04c9d14f8c8f69d655f2b00deb1df6fa
KTH-96 <taehyun960@gmail.com>
2021-10-19 19:44:25 +0900

    level1

---
+++ src/main/java/programmers/level1/num42862.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+public class num42862 {
+    public static void main(String[] args) {
+        int[] lost = {2, 4};
+        int[] resrve = {1,3,5};
+        int result = solution(5, lost, resrve);
+        System.out.println(result);
+    }
+    //n 학생수 lost 잃어버린 번호 reserve 여분 체육복 가지고있
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/main/java/programmers/level1/num76501.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+public class num76501 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]){
+                sum += absolutes[i];
+            }else{
+                sum -= absolutes[i];
+            }
+        }
+        return sum;
+    }
+}
