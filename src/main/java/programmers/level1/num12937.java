f388deb1c956712b3020901f6574c5d8391a5853
KTH-96 <taehyun960@gmail.com>
2021-12-02 16:44:00 +0900

    홀수 짝수

---
+++ src/main/java/programmers/level1/num12937.java
@@ -0,0 +1,14 @@
+package programmers.level1;
+
+public class num12937 {
+    public static void main(String[] args) {
+        num12937 main = new num12937();
+        main.solution(3);
+    }
+    public String solution(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        }
+        return "Odd";
+    }
+}
