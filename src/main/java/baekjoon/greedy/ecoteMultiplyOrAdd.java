159c7496943f2dbe49541c20914174cd64902fc9
KTH-96 <taehyun960@gmail.com>
2021-12-17 13:51:16 +0900

    이코테 곱하기 혹은 더하기

---
+++ src/main/java/baekjoon/greedy/ecoteMultiplyOrAdd.java
@@ -0,0 +1,22 @@
+package baekjoon.greedy;
+
+public class ecoteMultiplyOrAdd {
+    public static void main(String[] args) {
+        int solution = solution("02984");
+        System.out.println(solution);
+    }
+
+    public static int solution(String s) {
+        int firstNum = s.charAt(0) - '0';
+
+        for (int i = 1; i < s.length(); i++) {
+            int num = s.charAt(i) - '0';
+            if (num <= 1 || firstNum <= 1) {
+                firstNum += num;
+            }else {
+                firstNum *= num;
+            }
+        }
+        return firstNum;
+    }
+}
