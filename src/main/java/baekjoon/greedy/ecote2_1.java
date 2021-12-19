987d8f26f7e77e41c60de711f3f411b244263b77
KTH-96 <taehyun960@gmail.com>
2021-12-19 13:40:13 +0900

    이코테 상하좌우

---
+++ src/main/java/baekjoon/greedy/ecote2_1.java
@@ -0,0 +1,36 @@
+package baekjoon.greedy;
+
+public class ecote2_1 {
+    public static void main(String[] args) {
+        int size = 5;
+        String command = "R R R U D D";
+
+        int x = 1;
+        int y = 1;
+        String[] commandWord = command.split(" ");
+        for (int i = 0; i < commandWord.length; i++) {
+            if (commandWord[i].equals("R")) {
+                if (y == 5){
+                    continue;
+                }
+                y++;
+            } else if (commandWord[i].equals("L")) {
+                if (y == 1){
+                    continue;
+                }
+                y--;
+            }else if (commandWord[i].equals("U")) {
+                if (x == 1){
+                    continue;
+                }
+                x--;
+            }else if (commandWord[i].equals("D")) {
+                if (x == 5){
+                    continue;
+                }
+                x++;
+            }
+            System.out.println(x + " " + y);
+        }
+    }
+}
