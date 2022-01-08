0c97f3a7cf4f6ddecf381be775f37ebed86b7bd4
KTH-96 <taehyun960@gmail.com>
2022-01-08 13:33:53 +0900

    프로그래머스 타겟넘버

--- src/main/java/programmers/level2/num43165.java
+++ src/main/java/programmers/level2/num43165.java
@@ -1,8 +1,32 @@
 package programmers.level2;
 
 public class num43165 {
+    static int count;
+
+    public static void main(String[] args) {
+        num43165 main = new num43165();
+        int[] numbers = {1, 1, 1, 1, 1};
+        main.solution(numbers, 3);
+
+    }
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        return answer;
+        if (numbers.length == 0) return 0;
+        if (numbers.length == 1) return 1;
+        dfs(numbers, 0, 0, target);
+        return count;
+    }
+
+    private void dfs(int[] numbers, int dept, int sum, int target) {
+        if (dept == numbers.length) {
+            if (sum == target) {
+                count++;
+            }
+            return;
+        }
+        sum += numbers[dept];
+        dfs(numbers, dept + 1, sum, target);
+        sum -= numbers[dept];
+        sum += (-1 * numbers[dept]);
+        dfs(numbers, dept + 1, sum, target);
     }
 }
