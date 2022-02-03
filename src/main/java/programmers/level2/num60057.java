0ddb4b55742969fe49a1d430c2a10ad3b8a67509
KTH-96 <taehyun960@gmail.com>
2022-02-03 23:04:18 +0900

    2020 문자열 압축 성공 s의 길이가 1일때와 중간에 패턴을 변경해주는 부분 생성

--- src/main/java/programmers/level2/num60057.java
+++ src/main/java/programmers/level2/num60057.java
@@ -2,31 +2,35 @@ package programmers.level2;
 
 public class num60057 {
     public int solution(String s) {
-        int min = 1001;
-        if (s.length() == 1 ) return 1;
+        int min = 1001;// s의 최대 길이가 1000임
+        if (s.length() == 1) {
+            return 1;
+        }
+        //s의 1/2길이만큼만
         for (int i = 1; i <= s.length() / 2; i++) {
-            int count = 0;
-            String temp = s.substring(0, i);
-            String number = "";
-            for (int j = i; j < s.length(); j+=i) {
-                if (temp.equals(s.substring(j, j + i))) {
+            String pat = s.substring(0, i); // 패턴 문자열
+            int count = 1;
+            String str = "";
+            for (int j = i; j <= s.length() - i; j+=i) {
+                if (pat.equals(s.substring(j, j + i))){
                     count++;
-                } else {
-                        if (count > 1) {
-                            number += String.valueOf(count);
-                        }
-                        number += temp;
-                        count = 1;
+                }else {
+                    if (count > 1) {
+                        str += String.valueOf(count);
+                    }
+                    str += pat;
+                    pat = s.substring(j, j + i);
+                    count = 1;
                 }
             }
+            //s % j 남은거 는 따로 해줘야함
             if (count > 1) {
-                number += String.valueOf(count);
+                str += String.valueOf(count);
             }
-            number += temp;
+            str += pat;
             int rest = s.length() % i;
-            min = Math.min(min, number.length() + rest);
+            min = Math.min(min, str.length() + rest);
         }
-
         return min;
     }
 }
