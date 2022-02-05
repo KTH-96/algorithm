741bd3f5a05d0187e98ee2fa0e3350388a2cc081
KTH-96 <taehyun960@gmail.com>
2022-02-05 23:54:12 +0900

    2020 괄호변환 실패

---
+++ src/main/java/programmers/level2/num60058.java
@@ -0,0 +1,52 @@
+package programmers.level2;
+
+public class num60058 {
+    static int c = 0;
+    public String solution(String p) {
+        if (p.equals("")) {
+            return p;
+        }
+        boolean tf = check(p);
+        String u = p.substring(0, c);
+        String v = p.substring(c);
+
+        if (tf) {
+            return u + solution(v);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("(");
+        sb.append(solution(v));
+        sb.append(")");
+
+        for (int i = 1; i < u.length() - 1; i++) {
+            if (u.charAt(i) == '(') {
+                sb.append(")");
+            }else {
+                sb.append("(");
+            }
+        }
+        return sb.toString();
+    }
+
+    private boolean check(String p) {
+        if (p.charAt(0) == ')'){
+            return false;
+        }
+        int count = 0;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '(') {
+                count++;
+            }else {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+            if (count == 0) {
+                c = i + 1;
+                break;
+            }
+        }
+        return true;
+    }
+}
