71d903be12db832b9849216c8b8c7d951d8b1c41
KTH-96 <taehyun960@gmail.com>
2022-02-08 23:25:57 +0900

    2021카카오 메뉴리뉴얼 초안 작성

--- src/main/java/programmers/cocoa/primeNum.java
+++
@@ -1,5 +0,0 @@
-package programmers.cocoa;
-
-public class primeNum {
-
-}
---
+++ src/main/java/programmers/level2/num72411.java
@@ -0,0 +1,11 @@
+package programmers.level2;
+
+public class num72411 {
+    public String[] solution(String[] orders, int[] course) {
+        //i++, i = 2일때부터 a~z까지 배열 만들어서 카운트
+        //초기화 후 다시 반복
+        //orders = size[19], size[i].length = 1~10  course = size[10], course[i] = 2~10
+        String[] answer = {};
+        return answer;
+    }
+}
