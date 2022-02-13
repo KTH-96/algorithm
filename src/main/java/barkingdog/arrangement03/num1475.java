bb5cb76350b7acdbf48e1d598313c3604ee5e198
KTH-96 <taehyun960@gmail.com>
2022-02-14 00:05:48 +0900

    바킹독 03배열 문제집 방번호

---
+++ src/main/java/barkingdog/arrangement03/num1475.java
@@ -0,0 +1,36 @@
+package barkingdog.arrangement03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num1475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String roomNums = br.readLine();
+
+        int[] arr = new int[10];
+
+        String[] roomNum = roomNums.split("");
+        for (int i = 0; i < roomNum.length; i++) {
+            arr[Integer.parseInt(roomNum[i])]++;
+        }
+
+        int sixOrNine = (arr[6] + arr[9]);
+        if (sixOrNine % 2 == 0) {
+            sixOrNine /= 2;
+        } else {
+            sixOrNine = sixOrNine / 2 + 1;
+        }
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 6 || i == 9) {
+                continue;
+            } else {
+                max = Math.max(max, arr[i]);
+            }
+        }
+        max = Math.max(max, sixOrNine);
+        System.out.println(max);
+    }
+}
