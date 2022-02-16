e7ebdd1912be5e905b48573429dee32acb089268
KTH-96 <taehyun960@gmail.com>
2022-02-16 11:34:00 +0900

    요기다가 하면 어떻게됨?# Please enter the commit message for your changes. Lines starting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("hello world");
+        System.out.println("test2");
     }
 }
