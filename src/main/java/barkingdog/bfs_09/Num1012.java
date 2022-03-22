6dbc1c7a4ca7155e25fc2435e6b9f470f6886908
KTH-96 <taehyun960@gmail.com>
2022-03-22 23:55:25 +0900

    바킹독 09 bfs 유기농 배추 배열 크기 실패

---
+++ src/main/java/barkingdog/bfs_09/Num1012.java
@@ -0,0 +1,65 @@
+package barkingdog.bfs_09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Num1012 {
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
+    static int[][] map;
+    static boolean[][] vist;
+    static Queue<int[]> q = new LinkedList<>();
+    static int m, n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test_case = Integer.parseInt(br.readLine());
+        for (int i = 0; i < test_case; i++) {
+            String[] split = br.readLine().split(" ");
+            m = Integer.parseInt(split[0]);
+            n = Integer.parseInt(split[1]);
+            map = new int[m][n];
+            vist = new boolean[m][n];
+            for (int j = 0; j < Integer.parseInt(split[2]); j++) {
+                String[] xy = br.readLine().split(" ");
+                int x = Integer.parseInt(xy[0]);
+                int y = Integer.parseInt(xy[1]);
+                map[x][y] = 1;
+            }
+            int count = 0;
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (map[j][k] == 1 && !vist[j][k]) {
+                        bfs(j, k);
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
+    private static void bfs(int x, int y) {
+        vist[x][y] = true;
+        q.offer(new int[]{x, y});
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            for (int j = 0; j < 4; j++) {
+                int nx = cx + dx[j];
+                int ny = cy + dy[j];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m){
+                    continue;
+                }
+                if (map[nx][ny] != 1 || vist[nx][ny]){
+                    continue;
+                }
+                vist[nx][ny] = true;
+                q.offer(new int[]{nx, ny});
+            }
+        }
+    }
+}
