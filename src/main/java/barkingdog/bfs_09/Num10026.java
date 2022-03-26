f96ae13ecddddc16755fa827b11dbb5c44dc05a3
KTH-96 <taehyun960@gmail.com>
2022-03-26 13:46:09 +0900

    바킹독 09 bfs 적록색약

--- src/main/java/barkingdog/bfs_09/Num10026.java
+++ src/main/java/barkingdog/bfs_09/Num10026.java
@@ -1,5 +1,76 @@
 package barkingdog.bfs_09;
 
-public class Num10026 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Num10026 {
+    static char[][] map = new char[101][101];
+    static boolean[][] visit = new boolean[101][101];
+    static int n;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            char[] chars = br.readLine().toCharArray();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = chars[j];
+            }
+        }
+        int one = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visit[i][j]) {
+                    bfs(i, j);
+                    one++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                visit[i][j] = false;
+                if (map[i][j] == 'G') {
+                    map[i][j] = 'R';
+                }
+            }
+        }
+
+
+        int two = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visit[i][j]) {
+                    bfs(i, j);
+                    two++;
+                }
+            }
+        }
+        System.out.println(one + " " + two);
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        visit[x][y] = true;
+        q.offer(new int[]{x, y});
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
+                    continue;
+                }
+                if (visit[nx][ny] || map[cur[0]][cur[1]] != map[nx][ny]) {
+                    continue;
+                }
+                visit[nx][ny] = true;
+                q.offer(new int[]{nx, ny});
+            }
+        }
+    }
 }
