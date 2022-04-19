08c1082a904915d13528f4a2a2ce517289ba5536
KTH-96 <taehyun960@gmail.com>
2022-04-19 21:51:01 +0900

    바킹독 0A recursion 곱셈

---
+++ src/main/java/barkingdog/recursion_0A/Num1629.java
@@ -0,0 +1,35 @@
+package barkingdog.recursion_0A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Num1629 {
+
+	private static long c;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		c = Long.parseLong(st.nextToken());
+
+		System.out.println(pow(a, b));
+	}
+
+	private static long pow(long a, long b) {
+		if (b == 1) {
+			return a % c;
+		}
+		long temp = pow(a, b / 2);
+		temp = temp * temp % c;
+		if (b % 2 == 0) {
+			return temp;
+		}
+		return temp * a % c;
+	}
+
+}
