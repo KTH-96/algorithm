db68254ddc3e7b6eeeb817e8290e142642b60c1a
KTH-96 <taehyun960@gmail.com>
2022-12-01 16:34:55 +0900

    프로그래머스 2레벨 프린터

--- src/main/java/programmers/level2/num42587.java
+++ src/main/java/programmers/level2/num42587.java
@@ -5,21 +5,23 @@ import java.util.PriorityQueue;
 
 public class num42587 {
     public int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> print = new PriorityQueue<>(Comparator.reverseOrder());
         int answer = 0;
-
-        for (int i = 0; i < priorities.length; i++) {
-            print.add(priorities[i]);
+        PriorityQueue<Integer> print = new PriorityQueue<>(Comparator.reverseOrder());
+        for (Integer priority : priorities) {
+            print.offer(priority);
         }
-        while (print.isEmpty()) {
+        while (!print.isEmpty()) {
             for (int i = 0; i < priorities.length; i++) {
                 if (priorities[i] == print.peek()) {
-
+                    print.poll();
+                    answer++;
+                    if (i == location) {
+                        return answer;
+                    }
                 }
-                print.poll();
-                answer++;
             }
         }
+
         return answer;
     }
 }
