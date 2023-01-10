04c2fd240c85f639485162c98831118f3e94f7c6
KTH-96 <taehyun960@gmail.com>
2023-01-10 19:19:37 +0900

    leet code 110

---
+++ src/main/java/leetcode/Lc110.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+public class Lc110 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;this.left = left;this.right = right;
+        }
+    }
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
