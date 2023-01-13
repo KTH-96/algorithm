ef0656aca1ef3b11c5b1ae03a10cbf52f6ae88c8
KTH-96 <taehyun960@gmail.com>
2023-01-14 02:28:08 +0900

    leet code 1038

---
+++ src/main/java/leetcode/Lc1038.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class Lc1038 {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
