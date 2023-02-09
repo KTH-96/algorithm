c6ede747c6af911788b4f1ac222c8ff5089b5ba5
KTH-96 <taehyun960@gmail.com>
2023-02-09 18:49:15 +0900

    프로그래머스 동적계획법

---
+++ src/main/java/programmers/level3/Num12971.java
@@ -0,0 +1,23 @@
+package programmers.level3;
+
+public class Num12971 {
+    public int solution(int sticker[]) {
+        if (sticker.length == 1) {
+            return sticker[0];
+        }
+        int[] dp1 = new int[sticker.length];
+        int[] dp2 = new int[sticker.length];
+        //sticker 의 길이가 홀수이면서 dp[0]을 사용했다면 마지막 값은 사용불가
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[0];
+        for (int i = 2; i < sticker.length - 1; i++) {
+            dp1[i] = Math.max(dp1[i - 1], sticker[i] + dp1[i - 2]);
+        }
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+        for (int i = 2; i < sticker.length; i++) {
+            dp2[i] = Math.max(dp2[i - 1], sticker[i] + dp2[i - 2]);
+        }
+        return Math.max(dp1[sticker.length - 2], dp2[sticker.length - 1]);
+    }
+}
